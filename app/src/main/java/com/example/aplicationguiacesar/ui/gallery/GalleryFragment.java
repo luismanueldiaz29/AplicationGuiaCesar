970f97a08c59712e0daae839e26611fc02339756
Luis Manuel Diaz <luismaprograma@gmail.com>
2020-03-30 11:44:37 -0500

    sitios de interes

--- app/src/main/java/com/example/aplicationguiacesar/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/aplicationguiacesar/ui/gallery/GalleryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,13 +24,7 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
